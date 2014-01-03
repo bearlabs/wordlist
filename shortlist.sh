e96e8418c8ac856bea0d1cedb9a6f846016b10bd
Xin Feng <fengxin@foxmail.com>
2014-01-02 22:32:51 -0800

    usability improvement for shortlist

--- shortlist.sh
+++ shortlist.sh
@@ -7,17 +7,13 @@ LLIST=$LIST.info
 MAKEDIR
 
 if [ ! -z $1 ]; then 
-	# article ID is provided in $1
-	if [[ ! "$1" == *[!0-9]* ]]; then
-		ID=$1
-		FILE="$DATA/posts/$ID.info"
-	# no article ID; but command: clean
-	elif [ x"$1" == x"reset" ] || [ x"$1" == x"r" ]; then
+	# command: clean
+	if [ "$1" == "reset" ] || [ "$1" == "r" ]; then
 		rm -rf $DATA
 		echo ""
 		echo "All Marks Reset"
 		echo ""
-	elif [ x"$1" == x"length" ] || [ x"$1" == x"l" ]; then
+	elif [ "$1" == "length" ] || [ "$1" == "l" ]; then
                 if [ ! -e $LIST ]; then
                         echo $LIST
                         exit 1
@@ -33,15 +29,38 @@ if [ ! -z $1 ]; then
                 done
                 IFS=$OLDIFS
 		echo $LEN
-	elif [ x"$1" == x"marked" ] || [ x"$1" == x"m" ]; then
+	elif [ "$1" == "old" ] || [ "$1" == "o" ]; then
+                if [ ! -e $LIST ]; then
+                        exit 1
+                fi
+                OLDIFS=$IFS
+                IFS=$'\n'
+                LINES="`cat $LIST`"
+                for line in $LINES; do
+                        ID=`echo $line | awk -F '=' '{print $1}'`
+                        DIC="`echo $line | awk -F '=' '{print $2}'`"
+                        if [ -z $DIC ]; then
+                                $SHOW $ID
+                                continue
+                        fi
+                        LEN=`$DIR/en/list.sh $ID -10`
+                        pos=0
+                        while [ $pos -lt $LEN ]; do
+                                pos=`expr $pos + 1`
+                                if [[ ! $DIC =~ (^| )$pos($| ) ]]; then
+                                        $SHOW $ID $pos ec
+                                fi
+                        done
+                done
+                IFS=$OLDIFS
+	else
 		if [ ! -e $LIST ]; then
-			echo $LIST
 			exit 1
 		fi
 		OLDIFS=$IFS
                 IFS=$'\n'
 		LINES="`cat $LIST`"	
-		if [ -z "$2" ]; then
+		if [ -z "$1" ]; then
 			for line in $LINES; do
 				ID=`echo $line | awk -F '=' '{print $1}'`
 				DIC="`echo $line | awk -F '=' '{print $2}'`"
@@ -52,8 +71,8 @@ if [ ! -z $1 ]; then
 				IFS=$'\n'
 			done
 			IFS=$OLDIFS
-		elif [[ ! "$2" == *[!0-9]* ]]; then
-                        POS=$2
+		elif [[ ! "$1" == *[!0-9]* ]]; then
+                        POS=$1
                         LEN=0
                         CUR=0
                         for line in $LINES; do
@@ -82,18 +101,40 @@ if [ ! -z $1 ]; then
                                 IFS=$'\n'
                         done
                         SET $LLIST LEN $LEN
-			if [ -z $3 ]; then
+			if [ -z $2 ]; then
                         	$SHOW $LID $LPOS
 			else
-				$SHOW $LID $LPOS $3
-				if [ "$3" == "unmark" ] || [ "$3" == "u" ]; then
+				$SHOW $LID $LPOS $2
+				if [ "$2" == "unmark" ] || [ "$2" == "u" ]; then
 					$SHOW $LID save
 					POS=`expr $POS - 1`
 					SET $LLIST POS $POS
 				fi
 			fi
                         IFS=$OLDIFS
-		elif [ "$2" == "next" ] || [ "$2" == "n" ]; then
+		elif [ "$1" == "head" ] || [ "$1" == "h" ]; then
+			LEN=`$DIR/$0 length`
+			POS=1
+			if [ "$2" == "mark" ] || [ "$2" == "m" ] || [ "$2" == "unmark" ] || [ "$2" == "u" ]; then
+                                exit 1
+                        fi
+                        clear
+                        echo "#($POS/$LEN)"
+                        echo ""
+                        $DIR/$0 $POS $2
+                        echo ""
+		elif [ "$1" == "tail" ] || [ "$1" == "t" ]; then
+			LEN=`$DIR/$0 length`
+                        POS=$LEN
+                        if [ "$2" == "mark" ] || [ "$2" == "m" ] || [ "$2" == "unmark" ] || [ "$2" == "u" ]; then
+                                exit 1
+                        fi
+                        clear
+                        echo "#($POS/$LEN)"
+                        echo ""
+                        $DIR/$0 $POS $2
+                        echo ""
+		elif [ "$1" == "next" ] || [ "$1" == "n" ]; then
 			LEN=`$DIR/$0 length`
 			POS=$(GET $LLIST POS)
 			if [ -z "$POS" ]; then
@@ -102,15 +143,15 @@ if [ ! -z $1 ]; then
                         if [ $POS -lt $LEN ]; then
                                 POS=`expr $POS + 1`
                         fi
-			if [ "$3" == "mark" ] || [ "$3" == "m" ] || [ "$3" == "unmark" ] || [ "$3" == "u" ]; then
+			if [ "$2" == "mark" ] || [ "$2" == "m" ] || [ "$2" == "unmark" ] || [ "$2" == "u" ]; then
 				exit 1
 			fi
 			clear
 			echo "#($POS/$LEN)"
 			echo ""
-                       	$DIR/$0 $1 $POS $3
+                       	$DIR/$0 $POS $2
 			echo ""
-                elif [ "$2" == "prev" ] || [ "$2" == "p" ]; then
+                elif [ "$1" == "prev" ] || [ "$1" == "p" ]; then
                         LEN=`$DIR/$0 length`
                         POS=$(GET $LLIST POS)
                         if [ -z "$POS" ]; then
@@ -119,44 +160,20 @@ if [ ! -z $1 ]; then
                         if [ $POS -gt 1 ]; then
                                 POS=`expr $POS - 1`
                         fi
-			if [ "$3" == "mark" ] || [ "$3" == "m" ] || [ "$3" == "unmark" ] || [ "$3" == "u" ]; then
+			if [ "$2" == "mark" ] || [ "$2" == "m" ] || [ "$2" == "unmark" ] || [ "$2" == "u" ]; then
 				exit 1
 			fi
                       	clear
                        	echo "#($POS/$LEN)"
                        	echo ""
-                       	$DIR/$0 $1 $POS $3
+                       	$DIR/$0 $POS $2
                        	echo ""
-		elif [ "$2" == "unmark" ] || [ "$2" == "u" ]; then
+		elif [ "$1" == "unmark" ] || [ "$1" == "u" ]; then
 			POS=$(GET $LLIST POS)
 			if [ -z "$POS" ]; then
 				POS=1
 			fi
-			$DIR/$0 $1 $POS unmark
+			$DIR/$0 $POS unmark
 		fi
-	elif [ x"$1" == x"passed" ] || [ x"$1" == x"p" ]; then
-		if [ ! -e $LIST ]; then
-                        exit 1
-                fi
-                OLDIFS=$IFS
-                IFS=$'\n'
-                LINES="`cat $LIST`"
-                for line in $LINES; do
-                        ID=`echo $line | awk -F '=' '{print $1}'`
-                        DIC="`echo $line | awk -F '=' '{print $2}'`"
-			if [ -z $DIC ]; then
-				$SHOW $ID
-				continue
-			fi
-			LEN=`$DIR/en/list.sh $ID -10`
-			pos=0
-			while [ $pos -lt $LEN ]; do
-				pos=`expr $pos + 1`
-				if [[ ! $DIC =~ (^| )$pos($| ) ]]; then
-					$SHOW $ID $pos ec	
-				fi
-			done
-                done
-                IFS=$OLDIFS
 	fi
 fi
